d50c193cd0b6a95a4fbe71c3644ff64333c714d8
netopir <neto_pir@hotmail.com>
2022-09-17 15:55:21 +0200

    Update iso.sh

--- iso.sh
+++ iso.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+FILE=/tmp/iso
+if [ -d "$FILE" ]; then
+    echo "$FILE is a directory."
+else 
+    echo "$FILE does not exist."
+		mkdir /tmp/iso
+fi
+
+#old
+#!/bin/bash
 if [[ ! -e /tmp/iso ]]; then
             mkdir /tmp/iso
 fi
