baefb8197d98f55b911ffcf69aeb7a9bb027d8e3
netopir <neto_pir@hotmail.com>
2022-09-17 15:56:54 +0200

    Update ziprar.sh

--- ziprar.sh
+++ ziprar.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+FILE1=/bin/rar
+if [ -f "$FILE1" ]; then
+    echo "$FILE1 exists."
+else 
+    echo "$FILE1 does not exist."
+	cp -rp /share/CACHEDEV1_DATA/Config/rar /bin/
+fi
+
+FILE2=/bin/unrar
+if [ -f "$FILE2" ]; then
+    echo "$FILE2 exists."
+else 
+    echo "$FILE2 does not exist."
+	cp -rp /share/CACHEDEV1_DATA/Config/unrar /bin/
+fi
+
+FILE3=/bin/zip
+if [ -f "$FILE3" ]; then
+    echo "$FILE3 exists."
+else 
+    echo "$FILE3 does not exist."
+	cp -rp /share/CACHEDEV1_DATA/Config/zip /bin/
+fi
+
+FILE4=/bin/unzip
+if [ -f "$FILE4" ]; then
+    echo "$FILE4 exists."
+else 
+    echo "$FILE4 does not exist."
+	cp -rp /share/CACHEDEV1_DATA/Config/unzip /bin/
+fi
+
+
+#OLD
+#!/bin/bash
+
 if [ ! -a /bin/rar ];
 then
 	echo "rar - File exists."
